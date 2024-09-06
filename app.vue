dda6b0b48325e1d16f5a6ae46baec4bbb5494feb
Jose M Molina <jose.molina@icims.com>
2024-09-06 20:53:17 +0200

    why

--- app.vue
+++ app.vue
@@ -10,7 +10,7 @@ useSeoMeta({
   ogTitle: 'Get to know me',
   description: 'Get to know me.',
   ogDescription: 'Get to know me.',
-  ogImage: 'https://example.com/image.png',
+  ogImage: 'https://example.com/image1.png',
   twitterCard: 'summary_large_image',
 });
 </script>
