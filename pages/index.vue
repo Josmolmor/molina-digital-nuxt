c5f972575dc9b362775d04c3ab6cdeb930e56103
Jose M Molina <jose.molina@icims.com>
2024-09-11 18:12:22 +0200

    avatar hover

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,10 @@
   <main>
     <div class="entry-text">
       <div class="intro">
-        <img src="~/assets/img/me.jpg" />
+        <div class="avatar-container">
+          <div class="avatar-bg"></div>
+          <img src="~/assets/img/me.jpg" />
+        </div>
         <p>
           <span class="name">{{ $t('intro_1') }}</span>
           <span class="role">{{ $t('intro_1_2') }}</span>
@@ -83,10 +86,59 @@ main {
   gap: 16px;
   margin-bottom: 32px;
 
-  img {
-    border-radius: 50%;
-    height: 64px;
-    width: 64px;
+  .avatar-container {
+    position: relative;
+
+    &:hover {
+      .avatar-bg {
+        transform: rotate(720deg);
+      }
+    }
+
+    .avatar-bg {
+      height: 66px;
+      width: 66px;
+      background-color: #c5b99e;
+      background-image: radial-gradient(
+          circle farthest-corner at top left,
+          rgba(225, 243, 97, 1) 0%,
+          rgba(225, 243, 97, 0) 50%
+        ),
+        radial-gradient(
+          circle farthest-side at top right,
+          rgba(181, 176, 177, 1) 0%,
+          rgba(181, 176, 177, 0) 10%
+        ),
+        radial-gradient(
+          circle farthest-corner at bottom right,
+          rgba(204, 104, 119, 1) 0%,
+          rgba(204, 104, 119, 0) 33%
+        ),
+        radial-gradient(
+          circle farthest-corner at top right,
+          rgba(155, 221, 240, 1) 0%,
+          rgba(155, 221, 240, 0) 50%
+        ),
+        radial-gradient(
+          ellipse at bottom center,
+          rgba(254, 43, 0, 1) 0%,
+          rgba(254, 43, 0, 0) 80%
+        );
+      border-radius: 50%;
+      will-change: transform;
+      transition: transform 0.5s ease-in-out;
+    }
+
+    img {
+      border-radius: 50%;
+      height: 64px;
+      width: 64px;
+      pointer-events: none;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
 
   p {
